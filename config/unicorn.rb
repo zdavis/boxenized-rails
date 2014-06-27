7657557b63fff36bbce5bc0cda88c5a449c3fc73
Zach Davis <zach@castironcoding.com>
2014-06-27 16:29:46 -0700

    [CHORE] Setting up unicorn config and start script; creating binstubs

---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+rails_env = ENV['RAILS_ENV'] || 'production'
+rails_root  = `pwd`.gsub("\n", "")
+
+if rails_env == 'development'
+  socket = "#{ENV['BOXEN_SOCKET_DIR']}/melange"
+  preload = false
+  processes = 2
+else
+  socket = "#{ENV['UNICORN_SOCKET_DIR']}/unicorn"
+  preload = true
+  processes = 6
+end
+
+pid               "#{rails_root}/tmp/pids/unicorn.pid"
+stderr_path       "#{rails_root}/log/unicorn.log"
+stdout_path       "#{rails_root}/log/unicorn.log"
+worker_processes  processes
+listen            socket, :backlog => 1024
+preload_app       preload
+timeout           600
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
